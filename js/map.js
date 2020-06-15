88e405b054315e5930c616b3397f971543b48eb4
unknown <kamila0990@gmail.com>
2020-06-15 23:32:57 +0100

    api google maps

---
+++ js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 52.435080,
+            lng: 16.921430
+        },
+        zoom: 8
+    });
+}
