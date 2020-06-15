ebc7279b463b54bb9d34faa1d71d2cc4045e5b77
unknown <kamila0990@gmail.com>
2020-06-15 23:59:16 +0100

    add googlemp img

--- js/map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    let map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: 52.435080,
-            lng: 16.921430
-        },
-        zoom: 8
-    });
-}
